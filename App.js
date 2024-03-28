ad906885c76f90f131a798b3be6260aef4c26537
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-03-28 08:37:28 -0300

    AstronautaProjeto

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, Image } from 'react-native';
+import { styles } from './AppStyle';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Astronauta</Text>
+      <Image style= {styles.imagem}
+      source={require('./assets/astronauta.jpg')}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
